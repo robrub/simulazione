afe4ed7b139dc08d96f365f3e6308d7244ceeabb
iiamwendy <widadtaouaf03@gmail.com>
2024-01-26 14:17:02 +0100

    part1

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import Root from "./routes/root";
+import axios from'axios';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import ErrorPage from "./pages/ErrorPage";
+import { ChakraProvider } from "@chakra-ui/react";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+
+  },
+]);
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+   < ChakraProvider>
+    <RouterProvider router={router} />
+  </ChakraProvider>
+  </React.StrictMode>
+);
