8d6d4b6562ae8def386fd9ba460d3f96fab0fae4
Leonardo Apostolo <leonardo.apostolo@outlook.com>
2024-01-26 10:15:29 +0100

    feat: part-1

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import { ChakraProvider } from "@chakra-ui/react";
+
+import App from "./App.tsx";
+import ErrorPage from "./error-page.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider>
+      <RouterProvider router={router} />
+    </ChakraProvider>
+  </React.StrictMode>
+);
