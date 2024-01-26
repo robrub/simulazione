3bcb8a7264a82d8186e063d226820c4399b98eb3
Enricove <enricovegliani@gmail.com>
2024-01-26 12:10:08 +0100

    created getMovies and getDetailMovie funcions

--- src/apiCalls.tsx
+++ src/apiCalls.tsx
@@ -1,10 +1,13 @@
-const key : string = "efc30d58"
-const axios = require('axios').default;
-
-export function getMovies(){
-
+import axios from "axios";
+const instance = axios.create({
+    baseURL: "http://www.omdbapi.com/?apikey=efc30d58&",
+})
+export const getMovies = async () => {
+    const response = await instance.get("t=indiana+jones")
+    return response;
 }
 
-export function getDetailsMovie(id:number){
-
+export const getDetailsMovie = async (id:number) => {
+    const response = await instance.get("i="+id)
+    return response;
 }
